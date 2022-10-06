b1847ef4c5c25aeed48a02691f71eeaff7d1b640
zayarmyooo19 <zayarmyooo1832003@gmail.com>
2022-10-06 23:05:10 +0630

    login added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { Register } from './components/page/register/register.component';
 import { VerifyEmail } from './components/page/verifyEmail/verify-email.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Login } from './components/page/login/login.component';
 
 const routes: Routes = [
-  {path:'verifyemail',component:VerifyEmail},
+  {path:'verify-email',component:VerifyEmail},
   {path:'register',component:Register},
+  {path:'login',component:Login},
 ];
 
 @NgModule({
