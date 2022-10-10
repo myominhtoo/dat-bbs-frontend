b4d3ea4c5fef62aaf1f796711dac86002d2f953d
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-10-10 12:42:47 +0630

    added component logic in verify email page

--- src/app/components/page/verifyEmail/verify-email.component.ts
+++ src/app/components/page/verifyEmail/verify-email.component.ts
@@ -1,19 +1,36 @@
-import { Component } from  "@angular/core";
+import { Component , OnInit } from  "@angular/core";
 import { UserService } from "src/app/model/service/http/user.service";
 
 @Component({
     selector : 'verify-email',
     templateUrl:'./verify-email.component.html'
 })
-export class VerifyEmail{
-    email ='';
-    constructor(private userService : UserService ){
+export class VerifyEmail implements OnInit{
 
+    status = {
+        hasError : false,
+        isLoading : false,
+        hasGotVerification : false
     }
+
+    email ='';
+
+    constructor(private userService : UserService ){}
+
+    ngOnInit(): void {
+        this.status.hasGotVerification = localStorage.getItem('hasGotVerification') == 'true' ? true : false;
+    }
+
     onSubmit(){
+       this.status.isLoading = true;
        this.userService.sendVerification( this.email).subscribe({
         next : (res) => {
-            console.log(res);
+            this.status.hasError = !res.ok;
+            if( res.ok ) {
+                this.status.hasGotVerification = true;
+                localStorage.setItem('hasGotVerification',"true");
+            }
+            this.status.isLoading = false;
         }
        })
     }
--- src/app/components/page/verifyEmail/verifypass-component.ts
+++ src/app/components/page/verifyEmail/verifypass-component.ts
@@ -1,7 +1,7 @@
 import { Component } from  "@angular/core";
 
 @Component({
-    selector : 'verifypass',
+    selector : 'verify-pass',
     templateUrl:'./verifypass-component.html'
 })
 export class Verifypass{
