0940bce70c28e99d54a863ee5099b173c7866f40
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-10-12 21:42:05 +0630

    started create-board

--- src/app/components/page/board/create-board.component.ts
+++ src/app/components/page/board/create-board.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Board } from "src/app/model/bean/board";
 import { ToggleStore } from "src/app/model/service/store/toggle.service";
 
 @Component({
@@ -11,6 +12,8 @@ export class CreateBoardComponent{
 
     emailStr :string ='';
     emails : string [] = [];
+
+    board : Board = new Board();
   
     status = {
       update : {
@@ -22,45 +25,45 @@ export class CreateBoardComponent{
         msg : ''
       }
     }
-  }
   
-  onChange( event : KeyboardEvent ){
-    let emailStr = this.emailStr; // this ko ma thone chin loh
-    let lastChar = emailStr[emailStr.length - 1];
-    this.status.error = { hasError : false , msg : ''}
-    if( lastChar === ',' || lastChar === ' ' || event.keyCode == 13 ){
-      // previous char before last char
-      let prevLastChar = emailStr[emailStr.length - 2];
-
-      if( prevLastChar  == ' ' || prevLastChar == ','){
-        this.emailStr = prevLastChar == ' ' ? emailStr.trim() : emailStr.replaceAll(',','');
-      }
-      
-      if(emailStr.includes('@') && emailStr.includes('.')){
-        let storeEmail = emailStr.includes(',') ? emailStr.replaceAll(',','') : emailStr.replaceAll(' ','');
-        this.status.update.willUpdate
-        ? this.emails[this.status.update.idx] = emailStr
-        : this.emails.includes(storeEmail)
-         ? this.status.error = { hasError : true , msg : 'This email has already included!' }
-         : this.emails.push( storeEmail );
-
-        this.status.update = { idx : 0 , willUpdate : false }
-        this.emailStr = this.status.error.hasError ? this.emailStr : '';
-
-      }else{
-
-        this.status.error = { hasError : true , msg : 'Invalid email!'}
+    onChange( event : KeyboardEvent ){
+      let emailStr = this.emailStr; // this ko ma thone chin loh
+      let lastChar = emailStr[emailStr.length - 1];
+      this.status.error = { hasError : false , msg : ''}
+      if( lastChar === ',' || lastChar === ' ' || event.keyCode == 13 ){
+        // previous char before last char
+        let prevLastChar = emailStr[emailStr.length - 2];
+  
+        if( prevLastChar  == ' ' || prevLastChar == ','){
+          this.emailStr = prevLastChar == ' ' ? emailStr.trim() : emailStr.replaceAll(',','');
+        }
         
+        if(emailStr.includes('@') && emailStr.includes('.')){
+          let storeEmail = emailStr.includes(',') ? emailStr.replaceAll(',','') : emailStr.replaceAll(' ','');
+          this.status.update.willUpdate
+          ? this.emails[this.status.update.idx] = emailStr
+          : this.emails.includes(storeEmail)
+           ? this.status.error = { hasError : true , msg : 'This email has already included!' }
+           : this.emails.push( storeEmail );
+  
+          this.status.update = { idx : 0 , willUpdate : false }
+          this.emailStr = this.status.error.hasError ? this.emailStr : '';
+  
+        }else{
+  
+          this.status.error = { hasError : true , msg : 'Invalid email!'}
+          
+        }
       }
+    } 
+    removeEmail( idx : number ){
+      this.emails.splice(idx,1);
     }
-  }
-
-  removeEmail( idx : number ){
-    this.emails.splice(idx,1);
-  }
-
-  setUpdateEmail( idx : number ){
-    this.emailStr = this.emails[idx];
-    this.status.update = { idx : idx , willUpdate : true }
-  }
+  
+    setUpdateEmail( idx : number ){
+      this.emailStr = this.emails[idx];
+      this.status.update = { idx : idx , willUpdate : true }
+    }
+  
+  
 }
