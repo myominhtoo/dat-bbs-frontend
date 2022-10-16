b1ca7a6648cea8c0c9b39700d51a14c829801dab
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-10-16 22:04:03 +0630

    fixed net req in boards

--- src/app/components/page/board/my-board.component.ts
+++ src/app/components/page/board/my-board.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Stage } from "src/app/model/bean/stage";
+import { TaskCard } from "src/app/model/bean/taskCard";
 import { ToggleStore } from "src/app/model/service/store/toggle.service";
 
 @Component({
@@ -7,7 +9,12 @@ import { ToggleStore } from "src/app/model/service/store/toggle.service";
     templateUrl : './my-board.component.html'
 })
 export class MyBoardComponent{
+
+    stage : Stage [] = [];
+    tasks : TaskCard [] = [];
+
     constructor( public toggleStore : ToggleStore , public route : ActivatedRoute ){
         console.log(route.snapshot.params['id'])
     }
+
 }
---
+++ src/app/model/service/store/board.store.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import { Board } from "../../bean/board";
+import { BoardService } from "../http/board.service";
+
+@Injectable({
+    providedIn : 'root'
+})
+export class BoardStore{
+    public boards : Board [] = [];
+    public status = {
+        isLoading : true
+    }
+
+    constructor( private boaredService : BoardService ){
+        this.getBoardsByUserId( 1 );
+    }
+
+    private getBoardsByUserId( userId : number ){
+        this.status.isLoading = true;
+        this.boaredService.getBoardsForUser( userId )
+        .subscribe({
+            next : datas => {
+                this.status.isLoading = false;
+                this.boards = datas;
+                console.log('running')
+            },
+            error : err => {
+                console.log(err);
+            }
+        });
+    }
+
+}
