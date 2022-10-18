21c86751218de3cbbc28e0e8e4566536b69fd820
SuNandarAung1211 <sunandaraung1211@gmail.com>
2022-10-18 20:49:13 +0630

    Email invite in myBoardComponent

--- src/app/components/page/board/my-board.component.ts
+++ src/app/components/page/board/my-board.component.ts
@@ -7,6 +7,7 @@ import { BoardService } from "src/app/model/service/http/board.service";
 import { StageService } from "src/app/model/service/http/stage.service";
 import { BoardStore } from "src/app/model/service/store/board.store";
 import { ToggleStore } from "src/app/model/service/store/toggle.service";
+import swal from 'sweetalert';
 
 @Component({
     selector : 'my-board',
@@ -17,11 +18,26 @@ export class MyBoardComponent implements OnInit {
     public stages : Stage [] = [];
     tasks : TaskCard [] = [];
     board : Board = new Board();
+    email : string ='';
+    emails :string[] =[];
+    filterEmails : string [] = [];
+    storedEmails : string [] = [];
 
     status = {
         isLoading : false,
         isAddStage : false,
-        isAddingStage : false
+        isAddingStage : false,
+        isInviting : false,
+        error : {
+            email : {
+                hasError : false,
+                msg : ''
+              }
+        },
+        update : {
+            idx : 0,
+            willUpdate : false
+          }
     }
     // @Input('data') data : Stage = new Stage();
 
@@ -29,7 +45,9 @@ export class MyBoardComponent implements OnInit {
          public route : ActivatedRoute ,
          private router : Router , 
          private stageService : StageService ,
-         private boardService : BoardService  ){}
+         private boardService : BoardService  ){
+            
+         }
 
     ngOnInit(): void {
         this.doActionForCurrentBoard( this.route.snapshot.params['id'] );
@@ -83,5 +101,89 @@ export class MyBoardComponent implements OnInit {
     toggleIsAddStage(){
         this.status.isAddStage = !this.status.isAddStage;
     }
+
+    saveData(){
+        this.board.invitedEmails=this.emails;
+        this.status.isInviting = true;
+
+        
+        console.log(this.emails)
+   
+        swal({
+            text : 'Are you sure to invite this members?',
+            icon : 'warning',
+            buttons : [ 'No' , 'Yes' ]
+          }).then( isYes => {
+            if( isYes ){
+                this.boardService.getBoardWithEmail(this.board.id, this.board)
+              .subscribe({
+                next : data => {
+                  this.status.isInviting = false; 
+                  swal({
+                    text : 'successfully Invited !',
+                    icon : 'success'
+                  })
+                },
+                error : err => {
+                  console.log(err);
+                  this.status.isInviting =false;
+                }
+              });  
+            }else{
+              this.status.isInviting = false;
+            }
+          })
+    }
+
+    onChange( event : KeyboardEvent ){
+       
+        this.email  == ''
+        ? this.filterEmails = []
+        : this.filterAutoCompleteEmails( this.email);
+  
+  
+        let email = this.email;
+        let lastChar = email[email.length - 1];
+        this.status.error.email = { hasError : false , msg : ''}
+        if( lastChar === ',' || lastChar === ' ' ){
+          let prevLastChar = email[email.length - 2];
+    
+          if( prevLastChar  == ' ' || prevLastChar == ','){
+            this.email= prevLastChar == ' ' ? email.trim() : email.replaceAll(',','');
+          }
+          
+          if(email.includes('@') && email.includes('.')){
+            let storeEmail = email.includes(',') ? email.replaceAll(',','') : email.replaceAll(' ','');
+            this.status.update.willUpdate
+            ? this.emails[this.status.update.idx] = email
+            : this.emails.includes(storeEmail)
+             ? this.status.error.email = { hasError : true , msg : 'This email has already included!' }
+             : this.emails.push( storeEmail );
+    
+            this.status.update = { idx : 0 , willUpdate : false }
+            this.email = this.status.error.email.hasError ? this.email : '';
+    
+          }else{
+    
+            this.status.error.email = { hasError : true , msg : 'Invalid email!'}
+            
+          }
+        }
+      }
+      filterAutoCompleteEmails(   filterEmail : string ){
+        this.filterEmails =  this.storedEmails.filter(
+          ( email )=>{
+            return email.toLocaleLowerCase().includes( filterEmail.toLocaleLowerCase());
+          }
+        )
+      }
+      removeEmail( index : number ){
+        if(!this.status.isLoading) this.emails.splice(index,1);
+      }
+      updateEmail( index : number ){
+        this.email = this.emails [index];
+        this.status.update = { idx : index , willUpdate : true }
+        console.log(index)
+      }
 
 }
