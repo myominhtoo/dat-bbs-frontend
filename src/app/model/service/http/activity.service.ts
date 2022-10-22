78bbd6628541a3afc80c10c3cef5cfb3b34c428f
SuNandarAung1211 <sunandaraung1211@gmail.com>
2022-10-22 20:38:52 +0630

    added activity service

---
+++ src/app/model/service/http/activity.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Activity } from "../../bean/activity";
+
+@Injectable({
+    providedIn : 'root'
+})
+export class ActivityService{
+    constructor ( private httpClient : HttpClient ){}
+
+    createTaskCard( activity : Activity ) : Observable <HttpResponse<Activity>>{
+        return this.httpClient.post<HttpResponse<Activity>> (`http://localhost:8080/api/create-activity` , activity);
+    }
+
+    getActivities( taskCardId : number ) : Observable<Activity[]> {
+        return this.httpClient.get<Activity[]> (`http://localhost:8080/api/task-card/${taskCardId}/activities`);
+    }
+}
