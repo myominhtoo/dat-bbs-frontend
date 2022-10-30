efe8c7e8405c561e9a5c4cdefac887b75096502b
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-10-30 20:49:34 +0630

    re added edit button in stage title

--- src/app/components/page/board/my-board.component.ts
+++ src/app/components/page/board/my-board.component.ts
@@ -298,7 +298,6 @@ export class MyBoardComponent implements OnInit {
        stages.forEach( stage => {
         this.taskCardsMap.set( stage.stageName , [] );
        })
-       console.log(taskCards)
        taskCards.forEach( taskCard => {
          let prevTaskCards = this.taskCardsMap.get(taskCard.stage.stageName);
          prevTaskCards?.push(taskCard);
