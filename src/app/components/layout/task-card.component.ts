f8e54411a07ec28135a09f8298b6595ecc470d79
zayarmyooo19 <zayarmyooo1832003@gmail.com>
2022-11-04 13:56:25 +0630

    before conflict

--- src/app/components/layout/task-card.component.ts
+++ src/app/components/layout/task-card.component.ts
@@ -1,22 +1,46 @@
+import { COLORS } from './../../model/constant/colors';
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { TaskCard } from "src/app/model/bean/taskCard";
 import { OnInit } from '@angular/core';
-import { Board } from "src/app/model/bean/board";
 
 @Component({
     selector : 'task-card',
     template : `
-     <div (click)="handleShowOffCanvas( task)" class="d-flex flex-column align-items-start task-cards my-1 px-2 py-2 gap-3 text-muted shadow-sm bg-pale-snow">
-        <h5 class="fw-light h5">{{ task.taskName | titlecase }}</h5>
-            <!-- <span data-bs-toggle="dropdown">
-                <i class="fa-solid fa-ellipsis-vertical"></i>
-            </span> -->
-        <div class="w-100 d-flex justify-content-end gap-2 ">
-            
-            <span>{{ task.startedDate }}</span>
-            <span *ngIf="task.startedDate != task.endedDate">-</span>
-            <span *ngIf="task.startedDate != task.endedDate">{{ task.endedDate }}</span>
-        </div>
+     <div (click)="handleShowOffCanvas( task)" class="d-flex task-cards my-0 p-2 gap-2 text-muted shadow-sm bg-pale-snow" style="padding-right:10px;border-left:4px solid blue;">
+       <div class="d-flex flex-column align-items-star gap-3 w-100" >
+        <div class="d-flex justify-content-between w-100">
+                <h5 class="fw-light h5">{{ task.taskName | titlecase }}</h5>
+                <div class="d-flex gap-1 align-items-center">
+                    <div class="dropdown" id="color-dropdown">
+                        <i (click)="handleShowColorPlatte($event)" class="fa-solid fa-palette" data-bs-toggle="dropdown" data-bs-target="#color-dropdown"></i>                           
+                        <ul class="dropdown-menu p-3">
+                            <li style="font-size:14px;">Select Color :</li>
+                            <li class="d-flex flex-wrap gap-1 my-2">
+                            <p class="p-0 m-0 rounded-0 shadow-sm text-center" style="width:20px;height:20px;" ><i class="fa-solid fa-ban text-muted"></i></p>
+                                <p *ngFor="let color of colors" class="p-0 m-0 rounded-0" style="width:20px;height:20px;" [style]="'background:'+color" ></p>
+                            </li>
+                        </ul>   
+                    </div>   
+                    <i class="fas fa-solid fa-minus-circle text-danger d-none " class="task-del-btn"></i>
+                </div>
+            </div>
+
+                <!-- <span data-bs-toggle="dropdown">
+                    <i class="fa-solid fa-ellipsis-vertical"></i>
+                </span> -->
+            <div class="w-100 d-flex justify-content-end gap-2 ">
+                <div class="fs-6">
+                <span>{{ task.startedDate }}</span>
+                <span *ngIf="task.startedDate != task.endedDate">-</span>
+                <span *ngIf="task.startedDate != task.endedDate">{{ task.endedDate }}</span>
+                </div>
+                <div>
+                
+                    
+                </div>
+            </div>
+       </div> 
+      
      </div>
     `
 })
@@ -26,11 +50,18 @@ export class TaskCardComponent implements OnInit {
     //@Input('board') board : Board= new Board();
     @Output('show-task') showTask = new EventEmitter<TaskCard>();
 
+    colors : string [] = COLORS;
+
     ngOnInit() : void {
         // console.log((this.task.startedDate as unknown as Array<number>).toString())
     }
 
     handleShowOffCanvas( task : TaskCard ){
         this.showTask.emit( task );
+    }
+
+    handleShowColorPlatte( e  : Event ){
+        e.stopPropagation();
+
     }
 }
