1014fe4d6fe343d44868926189e33343cbf32e6d
SuNandarAung1211 <sunandaraung1211@gmail.com>
2022-11-06 22:29:51 +0630

    added forget password html

---
+++ src/app/components/page/forgetPassword/forget-password.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector :'forget-password',
+    templateUrl : './forget-password.component.html'
+})
+export class ForgetPasswordComponent implements OnInit {
+    ngOnInit(): void {
+        
+    }
+
+}
